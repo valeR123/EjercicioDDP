d410f0f6bf94eb7ef02e341df8c2776076e27364
valeR123 <71050449+valeR123@users.noreply.github.com>
2023-05-04 17:28:26 -0500

    Commit Final

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+@app.get("/hello")
 def Hello():
     return {"HELLO FASTAPI"}
 
+@app.get("/Isprime")
 def Isprime(numero): 
-    return False  
+     for n in range(2,numero):
+       if numero % n == 0:
+          return False  
+     return True
 
-
+@app.get("/Fibonnacci")
 def Fibonnacci(numero: int):
-     return 3
+     a=0
+     b=1
+     for n in range(0, numero):
+        c=a+b
+        a=b
+        b=c
+     return a
